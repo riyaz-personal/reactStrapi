220cd013ce77f47f8191930cb727f5a894be8103
riyaz-personal <riyazreengu@gmail.com>
2021-05-07 19:55:32 +0530

    check with hospital data from strapi

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // State of your application
+  state = {
+    restaurants: [],
+    error: null,
+  };
+
+  // Fetch your restaurants immediately after the component is mounted
+  componentDidMount = async () => {
+    try {
+      const response = await axios.get('http://localhost:1337/hospitals');
+      this.setState({ restaurants: response.data });
+    } catch (error) {
+      this.setState({ error });
+    }
+  };
+
+  render() {
+    const { error, restaurants } = this.state;
+
+    // Print errors if any
+    if (error) {
+      return <div>An error occured: {error.message}</div>;
+    }
+
+    return (
+      <div className="App">
+        <ul>
+          {restaurants.map(restaurant => (
+            <li key={restaurant.id}>{restaurant.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
